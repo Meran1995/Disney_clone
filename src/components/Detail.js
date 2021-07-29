340a5b16a7d6a0c229065774a6bd48eddd3768f1
meran <meranzakelijk@gmail.com>
2021-07-29 15:21:57 +0200

    started with the landing page script for the path to the movies

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,8 +7,21 @@ const Detail = (props) => {
 				<img
 					alt=''
 					src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6CDB6C0EB2D77EB19BCADA31F85066E001A1F61FA68F4AC3356A73FE076477F/scale?width=1440&aspectRatio=1.78&format=jpeg'
-					/>
+				/>
 			</Background>
+			<ImageTitle>
+				<img
+					alt=''
+					src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DDFF0FDF457E092EE53149CE7DB5BD14CB97E27B92D2D087E7C687B4E3073DE2/scale?width=1440&aspectRatio=1.78'
+				/>
+			</ImageTitle>
+			<ContentMeta>
+				<Controls>
+					<Player>
+						<img src="/images/play-icon-black.png" alt="" />PLAY
+					</Player>
+				</Controls>
+			</ContentMeta>
 		</Container>
 	);
 };
@@ -39,4 +52,72 @@ const Background = styled.div`
 		}
 	}
 `;
+
+const ImageTitle = styled.div`
+	align-text: flex-end;
+	display: flex;
+	-webkit-box-pack: start;
+	justify-content: flex-start;
+	margin: 0px auto;
+	height: 30vw;
+	min-height: 170px;
+	padding-bottom: 24px;
+	width; 100%;
+
+	img (
+		max-width: 600px;
+		min-width: 200px
+		width: 35vw;
+	)
+`;
+
+const ContentMeta = styled.div`
+	max-width: 874px;
+`;
+
+const Controls = styled.div`
+	align-items: center;
+	display: flex;
+	flex-flow: row nowrap;
+	margin: 24px;
+	min-height: 54px;
+`;
+
+const Player = styled.button`
+  font-size: 15px;
+  margin: 0px 22px 0px 0px;
+  padding: 0px 24px;
+  height: 56px;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transform: uppercase;
+  background: rgb (249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+
+  img {
+    width: 32px;
+  }
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+
+  @media (max-width: 768px) {
+    height: 45px;
+    padding: 0px 12px;
+    font-size: 12px;
+    margin: 0px 10px 0px 0px;
+	
+    img {
+      width: 25px;
+    }
+  }
+`;
+
 export default Detail;
